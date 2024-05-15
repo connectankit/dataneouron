b195d4917891bf4f9267fb52907a286c91bc4708
Ankit Kumar <70808441+akhacker0001@users.noreply.github.com>
2024-05-16 00:01:45 +0530

    done

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllDashboardData } = require('../controller');
+const { getAllDashboardData, create_component, update_component } = require('../controller');
 const router = express.Router()
 
 
 router.get('/dashboard',getAllDashboardData)
+router.post('/dashboard',create_component)
+router.put('/dashboard',update_component)
 
 
 module.exports = router
